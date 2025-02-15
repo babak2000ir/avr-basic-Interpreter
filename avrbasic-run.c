627db464ef836f1bdff5847d94a123d683b8b197
Babak Jahedmanesh <babak2000ir@gmail.com>
2025-02-15 14:11:45 +0000

    tokenizer fixed/tested

--- avrbasic-run.c
+++ avrbasic-run.c
@@ -9,24 +9,27 @@
 #define MAX_TOKENS 10
 #define MAX_TOKEN_LEN 6
 
-typedef struct {
+typedef struct
+{
     char name;
     int value;
 } Variable;
 
-typedef struct {
+typedef struct
+{
     int lineNumber;
     char returnValue[MAX_TOKEN_LEN];
     char command[MAX_TOKEN_LEN];
     char param1[MAX_TOKEN_LEN];
     char operator[MAX_TOKEN_LEN];
     char param2[MAX_TOKEN_LEN];
-    struct {
-        char command[MAX_TOKEN_LEN];
+    struct
+    {
+        char command1[MAX_TOKEN_LEN];
         char param1[MAX_TOKEN_LEN];
-        char operator[MAX_TOKEN_LEN];
+        char command2[MAX_TOKEN_LEN];
         char param2[MAX_TOKEN_LEN];
-    }  extension;
+    } extension;
 } ProgramLine;
 
 Variable variables[MAX_VARIABLES];
@@ -34,169 +37,314 @@ ProgramLine program[MAX_PROGRAM_LINES];
 int variableCount = 0;
 int programLength = 0;
 
-void loadProgram(const char* filename);
-void parseLine(char *buffer, ProgramLine *line);
-void tokenize(char *buffer);
+// Token Types Enum
+typedef enum
+{
+    TOKEN_UNKNOWN,
+    TOKEN_LINE_NUMBER,
+    TOKEN_COMMAND,
+    TOKEN_VARIABLE,
+    TOKEN_VALUE,
+    TOKEN_OPERATOR
+} TokenType;
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        printf("Usage: %s <program file>\n", argv[0]);
-        return 1;
+// Lists of reserved words
+const char *commands[32] = {"END", "CLEAR", "NEXT", "WEND", "DO", "GOTO", "WHILE", "UNTIL", "PRINT", "INPUT",
+                            "LET", "POKE", "FOR", "IF", "PEEK", "ABS", "SGN", "INT", "SQR", "EXP", "LOG", "LOG10",
+                            "SIN", "COS", "DEG", "RAD", "NOT", "SHL", "SHR", "TO", "THEN", "ELSE"};
+
+const char *operators[16] = {"=", "<>", "<", ">", "<=", ">=", "+", "-", "*", "/", "AND", "OR", "XOR", "MOD", ",", "^"};
+
+// Functions to check if the token is a reserved word
+void toUppercase(char str[])
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        str[i] = toupper(str[i]); // Convert each character
     }
-
-    loadProgram(argv[1]);
-    //executeProgram();
-    return 0;
 }
 
-void loadProgram(const char* filename) {
-    FILE* file = fopen(filename, "r");
-    if (!file) {
+int is_command(char *token)
+{
+    toUppercase(token);
+    for (int i = 0; i < sizeof(commands) / sizeof(commands[0]); i++)
+    {
+        if (strcmp(token, commands[i]) == 0)
+        {
+            return 1; // Match found
+        }
+    }
+    return 0; // No match found
+}
+
+int is_operator(char *token)
+{
+    toUppercase(token);
+    for (int i = 0; i < sizeof(operators) / sizeof(operators[0]); i++)
+    {
+        if (strcmp(token, operators[i]) == 0)
+        {
+            return 1; // Match found
+        }
+    }
+    return 0; // No match found
+}
+
+int j = 0; // Global variable j
+int init_j()
+{
+    j = 0; // Initialize j
+    return j;
+}
+int get_jpp()
+{
+    int prev = j; // Store the previous value
+    if (j == MAX_TOKEN_LEN - 1)
+    {
+        fprintf(stderr, "Error: A token on line %d exceeded MAX_TOKEN_LEN of %d\n", programLength + 1, MAX_TOKEN_LEN - 1);
+        exit(1);
+    }
+    j++; // Increment j
+    return prev;
+}
+int get_j()
+{
+    return j; // Return the current value of j
+}
+
+void tokenize(char *buffer, ProgramLine *line)
+{
+    int i = 0, tokenCounter = 0;
+    char token[MAX_TOKEN_LEN];
+    TokenType previousTokenType = TOKEN_UNKNOWN;
+
+    while (buffer[i] != '\0')
+    {
+        if (isspace(buffer[i]))
+        {
+            i++;
+            continue;
+        }
+
+        init_j(); // Initialize j
+
+        int negSign = ((previousTokenType == TOKEN_COMMAND || previousTokenType == TOKEN_OPERATOR) && buffer[i] == '-' && isdigit(buffer[i + 1]));
+
+        // Identifiers (simple version)
+        if (isalpha(buffer[i]))
+        {
+            while (isalnum(buffer[i]))
+            {
+                token[get_jpp()] = buffer[i++];
+            }
+            token[get_j()] = '\0';
+            tokenCounter++;
+
+            if (is_command(token))
+            {
+                previousTokenType = TOKEN_COMMAND;
+                printf("Command %i: %s\n", tokenCounter, token);
+            }
+            else
+            {
+                if (is_operator(token))
+                {
+                    previousTokenType = TOKEN_OPERATOR;
+                    printf("Operator %i: %s\n", tokenCounter, token);
+                }
+                else
+                {
+                    previousTokenType = TOKEN_VARIABLE;
+                    printf("Variable %i: %s\n", tokenCounter, token);
+                }
+            }
+        }
+        // Numbers
+        else if (isdigit(buffer[i]) || negSign)
+        {
+            if (negSign)
+            {
+                token[get_jpp()] = buffer[i++];
+            }
+            while (isdigit(buffer[i]))
+            {
+                token[get_jpp()] = buffer[i++];
+            }
+            token[get_j()] = '\0';
+
+            if (tokenCounter == 0)
+            {
+                previousTokenType = TOKEN_LINE_NUMBER;
+                printf("Line Number: %s\n", token);
+            }
+            else
+            {
+                tokenCounter++;
+                previousTokenType = TOKEN_VALUE;
+                printf("Value %i: %s\n", tokenCounter, token);
+            }
+        }
+        // Operators (simple single-character)
+        else if (strchr("+-*/=,><^", buffer[i]))
+        {
+            while (ispunct(buffer[i]))
+            {
+                char *stop = strchr("=,^+", buffer[i]);
+                token[get_jpp()] = buffer[i++];
+                if (stop)
+                {
+                    break;
+                }
+            }
+            token[get_j()] = '\0';
+            tokenCounter++;
+            previousTokenType = TOKEN_OPERATOR;
+            printf("Operator %i: %s\n", tokenCounter, token);
+        }
+        // Unknown character
+        else
+        {
+            printf("TOKEN_UNKNOWN %i: %c\n", tokenCounter, buffer[i]);
+            i++;
+            tokenCounter++;
+        }
+    }
+}
+
+void loadProgram(const char *filename, int lineNo)
+{
+    FILE *file = fopen(filename, "r");
+    if (!file)
+    {
         perror("Failed to open file");
         exit(1);
     }
 
     char buffer[100];
-    while (fgets(buffer, sizeof(buffer), file)) {
-        ProgramLine line;
-        tokenize(buffer);
-        //program[programLength++] = line;
+    while (fgets(buffer, sizeof(buffer), file))
+    {
+        if (lineNo > 0)
+        {
+            if (programLength == lineNo - 1)
+            {
+                ProgramLine line;
+                tokenize(buffer, &line);
+                program[programLength++] = line;
+                break;
+            }
+            else
+            {
+                programLength++;
+            }
+        }
+        else
+        {
+            ProgramLine line;
+            tokenize(buffer, &line);
+            program[programLength++] = line;
+        }
     }
 
     fclose(file);
 }
 
-void tokenize(char *buffer) {
-    int i = 0, j = 0;
-    char token[MAX_TOKEN_LEN];
-
-    while (buffer[i] != '\0') {
-        if (isspace(buffer[i])) {
-            i++;
-            continue;
-        }
-
-        j = 0;
-
-        // Identifiers (simple version)
-        if (isalpha(buffer[i])) {
-            while (isalnum(buffer[i])) {
-                token[j++] = buffer[i++];
-            }
-            token[j] = '\0';
-            printf("TOKEN_IDENTIFIER: %s\n", token);
-        }
-        // Numbers
-        else if (isdigit(buffer[i])) {
-            while (isdigit(buffer[i])) {
-                token[j++] = buffer[i++];
-            }
-            token[j] = '\0';
-            printf("TOKEN_NUMBER: %s\n", token);
-        }
-        // Operators (simple single-character)
-        else if (strchr("+-*/=;,", buffer[i])) {
-            token[0] = buffer[i++];
-            token[1] = '\0';
-            printf("TOKEN_OPERATOR: %s\n", token);
-        }
-        // Unknown character
-        else {
-            printf("TOKEN_UNKNOWN: %c\n", buffer[i]);
-            i++;
-        }
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        printf("Usage: %s <program file> [<line number>]\n", argv[0]);
+        return 1;
     }
+
+    int lineNo = 0;
+    if (argc == 3)
+    {
+        lineNo = atoi(argv[2]);
+    }
+
+    loadProgram(argv[1], lineNo);
+    // executeProgram();
+    return 0;
 }
 
-void parseLine(char *buffer, ProgramLine *line) {
-    char *tokens[MAX_TOKENS];
-    int tokenCount = 0;
-    
-    int lineNumber = 0;
-    if (!sscanf(buffer, "%d", &lineNumber) == 1) {
-        printf("Failed to extract number.\n");
-    }
-
-    char *delimiters = " ,";
-    char *token = strtok(buffer, delimiters);
-    while (token != NULL && tokenCount < MAX_TOKENS) {
-        tokens[tokenCount++] = token;
-        token = strtok(NULL, delimiters);
-    }
-    
-    if (tokenCount > 0) line->lineNumber = atoi(tokens[0]);
-    if (tokenCount > 1) strncpy(line->returnValue, tokens[1], sizeof(line->returnValue) - 1);
-    if (tokenCount > 2) strncpy(line->command, tokens[2], sizeof(line->command) - 1);
-    if (tokenCount > 3) strncpy(line->param1, tokens[3], sizeof(line->param1) - 1);
-    if (tokenCount > 4) strncpy(line->operator, tokens[4], sizeof(line->operator) - 1);
-    if (tokenCount > 5) strncpy(line->param2, tokens[5], sizeof(line->param2) - 1);
-    if (tokenCount > 6) strncpy(line->extension.command, tokens[6], sizeof(line->extension.command) - 1);
-    if (tokenCount > 7) strncpy(line->extension.param1, tokens[7], sizeof(line->extension.param1) - 1);
-    if (tokenCount > 8) strncpy(line->extension.operator, tokens[8], sizeof(line->extension.operator) - 1);
-    if (tokenCount > 9) strncpy(line->extension.param2, tokens[9], sizeof(line->extension.param2) - 1);
-}
-
-
-Variable* findVariable(char name) {
-    for (int i = 0; i < variableCount; i++) {
-        if (variables[i].name == name) {
+Variable *findVariable(char name)
+{
+    for (int i = 0; i < variableCount; i++)
+    {
+        if (variables[i].name == name)
+        {
             return &variables[i];
         }
     }
     return NULL;
 }
 
-void setVariable(char name, int value) {
-    Variable* var = findVariable(name);
-    if (var == NULL) {
-        if (variableCount < MAX_VARIABLES) {
+void setVariable(char name, int value)
+{
+    Variable *var = findVariable(name);
+    if (var == NULL)
+    {
+        if (variableCount < MAX_VARIABLES)
+        {
             variables[variableCount].name = name;
             variables[variableCount].value = value;
             variableCount++;
-        } else {
+        }
+        else
+        {
             printf("Variable limit exceeded\n");
         }
-    } else {
+    }
+    else
+    {
         var->value = value;
     }
 }
 
-int getVariableValue(char name) {
-    Variable* var = findVariable(name);
+int getVariableValue(char name)
+{
+    Variable *var = findVariable(name);
     return var ? var->value : 0;
 }
 
-void executeProgram() {
-    for (int i = 0; i < programLength; i++) {
+void executeProgram()
+{
+    for (int i = 0; i < programLength; i++)
+    {
         ProgramLine line = program[i];
-        if (strcmp(line.command, "LET") == 0) {
+        if (strcmp(line.command, "LET") == 0)
+        {
             int value = atoi(line.param2);
             setVariable(line.param1[0], value);
-        } else if (strcmp(line.command, "PRINT") == 0) {
+        }
+        else if (strcmp(line.command, "PRINT") == 0)
+        {
             int value = getVariableValue(line.param2[0]);
             printf("%d\n", value);
-        } else if (strcmp(line.command, "IF") == 0) {
+        }
+        else if (strcmp(line.command, "IF") == 0)
+        {
             int left = getVariableValue(line.param1[0]);
             int right = getVariableValue(line.param2[0]);
             int condition = 0;
-            if (strcmp(line.operator, "=") == 0) condition = (left == right);
-            else if (strcmp(line.operator, "<>") == 0) condition = (left != right);
-            else if (strcmp(line.operator, "<") == 0) condition = (left < right);
-            else if (strcmp(line.operator, ">") == 0) condition = (left > right);
-            else if (strcmp(line.operator, "<=") == 0) condition = (left <= right);
-            else if (strcmp(line.operator, ">=") == 0) condition = (left >= right);
+            if (strcmp(line.operator, "=") == 0)
+                condition = (left == right);
+            else if (strcmp(line.operator, "<>") == 0)
+                condition = (left != right);
+            else if (strcmp(line.operator, "<") == 0)
+                condition = (left < right);
+            else if (strcmp(line.operator, ">") == 0)
+                condition = (left > right);
+            else if (strcmp(line.operator, "<=") == 0)
+                condition = (left <= right);
+            else if (strcmp(line.operator, ">=") == 0)
+                condition = (left >= right);
 
-            if (condition) {
+            if (condition)
+            {
                 // Handle THEN and ELSE parts
             }
         }
         // Add more command handling here
     }
 }
-
-
-
-
-
-
-
--- sample.bas
+++ sample.bas
@@ -1,37 +1,56 @@
-10 END 
-20 CLEAR 
-30 NEXT 
-40 WEND 
-50 DO 
-60 GOTO Value 
-70 WHILE Value Opr Value
-80 UNTIL Value Opr Value 
-90 PRINT Value , Value 
-100 INPUT Value , Var
-110 LET Var = Value 
-120 POKE Value , Value 
-130 FOR Var = Value TO Value 
-140 IF Value Opr Value THEN Value ELSE Value
-150 Value = PEEK Value 
-160 Value = ABS Value 
-170 Value = SGN Value 
-180 Value = INT Value 
-190 Value = SQR Value 
-200 Value = EXP Value 
-210 Value = LOG Value 
-220 Value = LOG10 Value 
-230 Value = SIN Value 
-240 Value = COS Value 
-250 Value = DEG Value 
-260 Value = RAD Value 
-270 Value = NOT Value 
-280 Value = SHL Value , Value 
-290 Value = SHR Value , Value 
-300 Value = Value + Value 
-310 Value = Value - Value 
-320 Value = Value / Value 
-330 Value = Value * Value 
-340 Value = Value MOD Value 
-350 Value = Value AND Value 
-360 Value = Value OR Value 
-370 Value = Value XOR Value 
+1 END       
+2 CLEAR       
+3 NEXT       
+4 WEND       
+5 DO       
+6 GOTO 10      
+7 Goto a      
+8 WHILE var >= 10    
+9 UNTIL c = d    
+10 PRINT 0, 255     
+11 INPUT e , var    
+12 LET Var = 120    
+13 POKE 220, Value     
+14 FOR Var = 1 TO b  
+15 IF t <> 100 THEN 10 ELSE c
+16 a = PEEK 100    
+17 b = ABS b    
+18 c = SGN -100    
+19 d = INT 10    
+20 e = SQR a    
+21 f = EXP 10    
+22 g = LOG Value    
+23 h = LOG10 Value    
+24 i = SIN Value    
+25 Value = COS Value    
+26 Value = DEG Value    
+27 Value = RAD Value    
+28 Value = NOT Value    
+29 Value = SHL Value , 100  
+30 Value = SHR 20,20    
+31 Value = Value+Val2     
+32 Value = a-100     
+33 Value = Value / Value   
+34 Value = Value * Value   
+35 Value = Value MOD Value   
+36 Value = Value AND Value   
+37 Value = Value OR Value   
+38 Value = Value XOR Value
+39 c = SGN -100
+40 d = INT - 10
+41 e = a-b
+42 f = a- b
+43 g = a -b
+44 h = a - 10
+45 i = a -10
+46 Value = a-10
+47 GOTO -10
+48 GOTO - 10
+49  WHILE var >= -100 
+50  UNTIL c =-2
+51 PRINT 0, -255     
+52 INPUT e ,-10    
+53 LET Var =-120    
+54 POKE 220, Value     
+55 FOR Var = -10 TO -1  
+56 IF t <> -100 THEN 10 ELSE c
